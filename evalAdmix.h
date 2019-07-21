b1f0deb3fd62f77904d20ee422b9227cba3c662e
Genis Garcia Erill <genis.erill@bio.ku.dk>
2019-07-21 14:36:30 +0200

    fixed header file

--- evalAdmix.h
+++ evalAdmix.h
@@ -1,3 +1,3 @@
-void evalAdmix(double *cor, int ind1u, int ind1, double **r, double *mean_r, int **G, int K, double **Q, double **F, int nInd, int nSites,int nIts, int **isMissing, int nIndUse);
+void evalAdmix(double *cor, int ind1u, int ind1, double **r, double *mean_r, int **G, int K, double **Q, double **F, int nInd, int nSites,int nIts, int **isMissing, int nIndUse, int *useInd);
 
-void calcRes(double **r, double *mean_r, int **g, double **q, double **f, int K, int nSites, int nInd, int **isMissing);
+void calcRes(double **r, double *mean_r, int **G, double **Q, double **F, int K, int nSites, int nInd,int nIndUse,  int **isMissing, int *useInd);
