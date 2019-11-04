bd75cce533094e3c9ceac65d27a75560b7237329
Genis Garcia Erill <genis.erill@bio.ku.dk>
2019-11-04 16:09:44 +0100

    Fixed NaNs also for ngs, still dirty

--- ngsevalAdmix.cpp
+++ ngsevalAdmix.cpp
@@ -22,8 +22,8 @@ void adaptFandeG(double **F, double **Q, double **genos, int nSites, int nInd, i
 	double sumBG[K];
     
 	for(int k=0; k<K;k++){
-	  sumAG[k] = 0;
-	  sumBG[k] = 0;
+	  sumAG[k] = 1;
+	  sumBG[k] = 1;
 	}
 
 	for(int i=0; i<nInd;i++){
